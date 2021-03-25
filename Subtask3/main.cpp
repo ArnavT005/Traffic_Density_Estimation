3a12c92094bfe4e00c1d1e882481706690b876b9
ArnavT005 <58439450+ArnavT005@users.noreply.github.com>
2021-03-25 13:00:36 +0530

    Add optical flow

--- Subtask3/main.cpp
+++ Subtask3/main.cpp
@@ -8,10 +8,10 @@ using namespace chrono;
 
 
 //fbase = file baseline, f1 = file of method 1 ...
-//ofstream fbase("Baseline.txt"); 
-ofstream f1("M1_subSample.txt"), f2("M2_sparseDense.txt"), f3("M3_reduceResol.txt");
+//ofstream fbase("Baseline.txt"); f2("M2_sparseDense.csv"),  
+ofstream f1("M1_subSample.txt"), f3("M3_reduceResol.txt");
 ofstream f4("M4_spatialSplit.txt"), f5("M5_temporalSplit.txt"), futil("UtilityReport.txt", ios::app);
-ifstream fbase_in("Baseline.txt"), f1_in("M1_subSample.txt"), f2_in("M2_sparseDense.txt"), f3_in("M3_reduceResol.txt");
+ifstream fbase_in("Baseline.txt"), f1_in("M1_subSample.txt"), f2_in("M2_sparseDense.csv"), f3_in("M3_reduceResol.txt");
 ifstream f4_in("M4_spatialSplit.txt"), f5_in("M5_temporalSplit.txt");
 
 //rtbase = runtime baseline, rt1 = runtime of method 1 ...
@@ -19,7 +19,7 @@ ifstream f4_in("M4_spatialSplit.txt"), f5_in("M5_temporalSplit.txt");
 float rtbase, rt1, rt2, rt3, rt4, rt5;
 
 //method parameters
-int p1 = 5, p2, p3 = 100, p4 = 200, p5, p6;
+int p1 = 3, p2 = 1, p3 = 100, p4 = 200, p5, p6;
 
 //method utilities
 float u1, u2, u3, u4, u5;
@@ -27,7 +27,7 @@ float u1, u2, u3, u4, u5;
 float e1, e2, e3, e4, e5;
 
 //Set testmi to true to test method i
-bool testbase = false, testm1 = false, testm2 = false, testm3 = true, testm4 = false, testm5 = false;
+bool testbase = false, testm1 = false, testm2 = true, testm3 = false, testm4 = false, testm5 = false;
 
 //FUNCTIONS
 
@@ -110,10 +110,10 @@ float error(ifstream& base, ifstream& file, int x = 3) {
             getline(file, fileM);
             currentLine++;
         }
-
+         
         error += (stof(baseQ) - stof(fileQ)) * (stof(baseQ) - stof(fileQ)) + (stof(baseM) - stof(fileM)) * (stof(baseM) - stof(fileM));
 
-        cout << stof(baseQ) << " " << stof(fileQ) << " " << error << "\n";
+        //cout << stof(baseM) << " " << stof(fileM) << " " << error << "\n";
 
         baseLineNumber++;
     }
@@ -223,8 +223,159 @@ void M1_subSample(VideoCapture video, Mat background, Mat matrix, int x) {
     }
 }
 
-void M2_sparseDense(int x) {
+void M2_sparseDense(VideoCapture video, Mat background, Mat matrix, int x = 0) {
+    Mat frame1, frame2, thresh;
+    float AREA = background.size().area();
+    float denseQ = 0, denseM = 0, time;
+    int see_every_n_frame = 3, frame = 1;
 
+    //read Frame 1
+    video.read(frame1);
+    frame1 = warpAndCrop(frame1, matrix);
+    cvtColor(frame1, frame1, COLOR_BGR2GRAY);
+
+    if (x == 0) {
+        while (true) {
+
+            for (int i = 0; i < see_every_n_frame; i++) {
+                video.read(frame2);
+                frame++;
+            }
+
+            //Video End
+            if (frame2.empty()) {
+                break;
+            }
+
+            frame2 = warpAndCrop(frame2, matrix);
+
+            thresh = subImg(background.clone(), frame2.clone(), 40);
+            denseQ = findArea(thresh) / AREA;
+
+            cvtColor(frame2, frame2, COLOR_BGR2GRAY);
+
+            Mat flow(frame1.size(), CV_32FC2);
+
+            calcOpticalFlowFarneback(frame1, frame2, flow, 0.5, 3, 15, 3, 7, 1.5, 0);
+
+            Mat flow_parts[2];
+            split(flow, flow_parts);
+
+            Mat magnitude, angle, magn_norm;
+            cartToPolar(flow_parts[0], flow_parts[1], magnitude, angle, true);
+            normalize(magnitude, magn_norm, 0.0f, 1.0f, NORM_MINMAX);
+            angle *= ((1.f / 360.f) * (180.f / 255.f));
+
+            Mat _hsv[3], hsv, hsv8, bgr;
+            _hsv[0] = angle;
+            _hsv[1] = Mat::ones(angle.size(), CV_32F);
+            _hsv[2] = magn_norm;
+            merge(_hsv, 3, hsv);
+            hsv.convertTo(hsv8, CV_8U, 255.0);
+
+            cvtColor(hsv8, bgr, COLOR_HSV2BGR);
+            cvtColor(bgr, bgr, COLOR_BGR2GRAY);
+
+            threshold(bgr, bgr, 15, 255, THRESH_BINARY);
+
+            Mat kernel7x7 = getStructuringElement(MORPH_RECT, Size(7, 7));
+            Mat kernel15x15 = getStructuringElement(MORPH_RECT, Size(15, 15));
+
+            erode(bgr, bgr, kernel7x7);
+            dilate(bgr, bgr, kernel15x15);
+
+            denseM = findArea(bgr) / AREA;
+
+            // error correction if Queue density < Dynamic density
+            // slight error occurs due to different threshold values used
+            // when only moving vehicles are present
+            denseQ = denseQ > denseM ? denseQ : denseM;
+
+            // video is 15 FPS
+            time = (float)frame / 15;
+
+            //f2 << time << "," << denseQ << "," << denseM << "\n";
+
+            //update frame1 to frame2 and loop back
+            frame1 = frame2;
+        }
+    }
+    else {
+        vector<Scalar> colors;
+        RNG rng;
+        for (int i = 0; i < 100; i++) {
+            int r = rng.uniform(0, 256);
+            int g = rng.uniform(0, 256);
+            int b = rng.uniform(0, 256);
+            colors.push_back(Scalar(r, g, b));
+        }
+        vector<Point2f> p0, p1;
+        /*goodFeaturesToTrack(frame1, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+        Mat mask = Mat::zeros(frame1.size(), frame1.type());*/
+        while (true) {
+
+            for (int i = 0; i < see_every_n_frame; i++) {
+                video.read(frame2);
+                frame++;
+            }
+
+            //Video End
+            if (frame2.empty()) {
+                break;
+            }
+
+            goodFeaturesToTrack(frame1, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+            Mat mask = Mat::zeros(frame1.size(), frame1.type());
+
+            frame2 = warpAndCrop(frame2, matrix);
+
+            thresh = subImg(background.clone(), frame2.clone(), 40);
+            denseQ = findArea(thresh) / AREA;
+
+            cvtColor(frame2, frame2, COLOR_BGR2GRAY);
+
+            vector<uchar> status;
+            vector<float> err;
+
+            TermCriteria criteria = TermCriteria((TermCriteria::COUNT)+(TermCriteria::EPS), 10, 0.03);
+            calcOpticalFlowPyrLK(frame1, frame2, p0, p1, status, err, Size(15, 15), 2, criteria);   
+
+            vector<Point2f> good_new;
+
+            for (uint i = 0; i < p0.size(); i++) {
+                if (status[i] == 1) {
+                    good_new.push_back(p1[i]);
+                    line(mask, p1[i], p0[i], colors[i], 2);
+                    circle(frame2, p1[i], 5, colors[i], -1);
+                }
+            }
+
+            Mat img;
+            add(frame2, mask, img);
+
+            imshow("Flow", img);
+            waitKey(30);
+
+            //denseM = findArea(bgr) / AREA;
+
+            // error correction if Queue density < Dynamic density
+            // slight error occurs due to different threshold values used
+            // when only moving vehicles are present
+            denseQ = denseQ > denseM ? denseQ : denseM;
+
+            // video is 15 FPS
+            time = (float)frame / 15;
+
+            //f2 << time << "," << denseQ << "," << denseM << "\n";
+
+            //update frame1 to frame2 and loop back
+            frame1 = frame2.clone();
+            p0 = good_new;
+
+        }
+
+
+    }
 }
 
 void M3_reduceResol(VideoCapture video, Mat background, Mat matrix, int x, int y) {
@@ -326,13 +477,14 @@ int main() {
     //Write in text-file time | denseQ | denseM | frame number(optional). useful for graphing.
     if (testbase) { start = clock(); baseline(video, background.clone(), matrix); end = clock(); rtbase = float(end - start) / CLOCKS_PER_SEC; }
     if (testm1) { start = clock(); M1_subSample(video, background.clone(), matrix, p1); end = clock(); rt1 = float(end - start) / CLOCKS_PER_SEC; }
-    if (testm2) { start = clock(); M2_sparseDense(p2); end = clock(); rt2 = float(end - start) / CLOCKS_PER_SEC; }
+    if (testm2) { start = clock(); M2_sparseDense(video, background.clone(), matrix, p2); end = clock(); rt2 = float(end - start) / CLOCKS_PER_SEC; }
     if (testm3) { start = clock(); M3_reduceResol(video, background.clone(), matrix, p3, p4); end = clock(); rt3 = float(end - start) / CLOCKS_PER_SEC; }
     if (testm4) { start = clock(); M4_spatialSplit(p5); end = clock(); rt4 = float(end - start) / CLOCKS_PER_SEC; }
     if (testm5) { start = clock(); M5_temporalSplit(p6); end = clock(); rt5 = float(end - start) / CLOCKS_PER_SEC; }
 
     f1.close(); 
-    f2.close();  f3.close();  f4.close();  f5.close();
+    //f2.close(); 
+    f3.close();  f4.close();  f5.close();
 
     //Print utility report in text file. Print method name | parameter value | utility | time consumed in one line. used for debugging and changes.
     //Then after the above report print comma seperated utility, runtime for final graphing.
